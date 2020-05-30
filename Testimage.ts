f1237e344a96c68ab83f79a2b39e4f09e2e76dba
jacobdewey <66138103+jacobdewey@users.noreply.github.com>
2020-05-29 20:19:16 -0400

    Updates.

---
+++ Testimage.ts
@@ -0,0 +1,82 @@
+//% color=#1E90FF weight=100 icon="\uf150"
+namespace consolePrint {
+    //% block
+    export function TestDriveAny(s: any): void {
+        console.log(s)
+    }
+    //% block
+    export function TestDriveString(s: string): void {
+
+
+        consolePrint.TestDriveAny(s)
+    }
+    //% block
+    export function TestDriveNumber(s: number): void {
+
+
+        consolePrint.TestDriveAny(s)
+    }
+    //% block
+    export function TestDriveBoolean(s: boolean): void {
+
+
+        consolePrint.TestDriveAny(s)
+    }
+    //% block
+    export function TestDriveImage(s: Image): void {
+        consolePrint.TestDriveAny(s)
+    }
+
+
+
+
+
+
+
+    //% block
+    export function TestDriveValueAny(s: any): any {
+        return console.log(s)
+
+
+    }
+    //% block
+    export function TestDriveValueString(s: string): any {
+
+
+        consolePrint.TestDriveValueAny(s)
+    }
+
+
+    //% block
+    export function TestDriveValueNumber(s: number): any {
+
+
+        consolePrint.TestDriveValueAny(s)
+    }
+    //% block
+    export function TestDriveValueBoolean(s: boolean): any {
+
+
+        consolePrint.TestDriveValueAny(s)
+    }
+
+    //% block
+    export function clearLog(): void {
+
+
+        control.reset()
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
